29a443e549810ecf71ff6c466ea603ce30c155da
mariaruth1 <113031776+mariaruth1@users.noreply.github.com>
2023-03-23 12:49:08 +0100

    what is the purpose?

--- src/dk/easv/ImageViewerWindowController.java
+++ src/dk/easv/ImageViewerWindowController.java
@@ -1,18 +1,24 @@
 package dk.easv;
 
 import java.io.File;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
-public class ImageViewerWindowController
+public class ImageViewerWindowController implements Initializable
 {
     private final List<Image> images = new ArrayList<>();
     private int currentImageIndex = 0;
@@ -22,6 +28,14 @@ public class ImageViewerWindowController
 
     @FXML
     private ImageView imageView;
+
+    @FXML
+    private Slider sliderDelay;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 
     @FXML
     private void handleBtnLoadAction()
@@ -70,4 +84,24 @@ public class ImageViewerWindowController
             imageView.setImage(images.get(currentImageIndex));
         }
     }
+
+    public void handleBtnStartSlideshow(ActionEvent actionEvent) {
+
+    }
+
+    public void handleBtnStopSlideshow(ActionEvent actionEvent) {
+    }
+
+    public void handleSliderDelay(DragEvent dragEvent) {
+    }
+
+
+
+    //Start slideshow: All the images, which the user has selected from a folder,
+    // should be displayed as a slideshow.
+    // Each image should be displayed for a customizable number of seconds (between 1 and 5).
+    // Then the last image has been displayed,
+    // the slideshow should go back to the first image and continue the slideshow presentation.
+    // The slideshow presentation must be implemented using threads.
+    // Stop slideshow: This should stop the slideshow presentation.
 }
